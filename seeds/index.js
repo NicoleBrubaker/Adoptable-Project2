280845591624be811c91347167b33ee08953a8cb
Nicole Brubaker <nschumacher196@gmail.com>
2024-04-03 08:46:59 -0700

    added favorites seeds to start building database and saving feature

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,12 @@
 const sequelize = require("../config/connection");
-const seedUsers = require("./userdata.js")
+const seedUsers = require("./userdata.js");
+const seedFavorites = require("./favedata.js");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedUsers();
+  await seedFavorites();
 
   process.exit(0);
 };
