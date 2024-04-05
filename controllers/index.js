85a7704538cc325a45cf1747a7bb81a245f598f9
Nicole Brubaker <nschumacher196@gmail.com>
2024-04-04 20:52:38 -0700

    correcting routes folder structure, working on user sessions

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const homeRoutes = require("./home-routes");
 const apiRoutes = require("./api");
+const authRoutes = require("./auth");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.use("/auth", authRoutes);
 
 module.exports = router;
